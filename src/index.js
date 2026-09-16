3e79a8491a0a19337820897f0d72c466e80f01bc
jjmolomo17-tech <jjmolomo17@gmail.com>
2026-09-17 00:29:58 +0200

    index.js altered

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
+// src/index.js
+const express = require('express');
+const cors = require('cors');
+
+const app = express();   // <-- this line was missing
+app.use(cors());
+app.use(express.json());
+
+// Import routes
 const seatRoutes = require('./routes/seats');
 app.use('/api/seats', seatRoutes);
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
